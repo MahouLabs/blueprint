a112378200d5863e23184b5de423d889d55cc66b
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-02-07 11:00:54 -0300

    Update vite config with cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-import { unstable_vitePlugin as remix } from '@remix-run/dev';
+import {
+  unstable_cloudflarePreset as cloudflare,
+  unstable_vitePlugin as remix,
+} from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -6,5 +9,14 @@ import { remixDevTools } from 'remix-development-tools/vite';
 import remixConfig from './remix.config';
 
 export default defineConfig({
-  plugins: [remixDevTools(), remix({ ...remixConfig }), tsconfigPaths()],
+  plugins: [
+    remixDevTools(),
+    remix({ ...remixConfig, presets: [cloudflare()] }),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    resolve: {
+      externalConditions: ['workerd', 'worker'],
+    },
+  },
 });
